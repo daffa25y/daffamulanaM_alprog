8d1e76d6407e8473cb74a3a2645896f817351075
daffa25y <dafamaulana8632@gmail.com>
2025-11-07 15:54:24 +0800

    Add files via upload

---
+++ pert2.py
@@ -0,0 +1,17 @@
+#VARIABEL,cara kita menyimpan nilai
+
+#memberikan nilai
+x = 5.5
+y = 9
+luas = 100
+
+print("nilai dari x adalah :" , x)
+print("nilai dari y adalah :" , y)
+print("nilai dari luas adalah :" , luas)
+
+bet99 = 10  # tidak boleh dawali angka
+daffa_gantenng = 10 # jika2 suku kata, sambung dengan_
+jumlahHutang = 100 # jika 2 suku kata, sambung dengan  kapital
+
+# NAMA SAYA ADALAH ( DAFFA MAULANA MUHTASAR )
+# NIM ( 25241120 )
---
+++ pert3.py
@@ -0,0 +1,13 @@
+#type data
+#integer(bilangan bulat)
+#float(pecahan)
+
+x = 10 # integer
+y = 10.5 # float
+status = "jomblo" # string
+kebenaran = False #boolean (true false)
+
+print("nilai dari x=", x,"bertipe data", type(x))
+print("nilai dari x=", y,"bertipe data", type(y))
+print("nilai dari status =","bertipe data", type(status))
+print("nilai dari kebenaran =","bertipe data", type(kebenaran))
