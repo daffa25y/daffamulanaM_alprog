a26af584301058b7c2cfe321fd5ffa66e79ebddb
daffa25y <dafamaulana8632@gmail.com>
2025-11-14 15:13:23 +0800

    Add files via upload

---
+++ pert4.py
@@ -0,0 +1,28 @@
+# mengambil inputan dari user
+
+
+nama = input ("masukkan nama anda :")
+print("selamat datang" , nama)
+print(type(nama))
+
+umur = int(input ("masukkan umur anda :"))
+print("umur anda sekarang :" , umur)
+print(type(umur))
+
+TinggiBandan = input ("masukkan TinggiBandan anda :")
+print("TinggiBadan anda sekarang :" , TinggiBadan)
+print(type(TinggiBadan))
+
+hidup = input ("masukkan hidup anda :")
+print("hidup anda sekarang :" , hidup)
+print(type(hidup))
+
+
+
+nama = input ("masukkan nama anda :")
+print("selamat datang" , nama)
+print(type(nama))
+
+nim = int(input("masukkan nim anda :"))
+print("nim anda : " , nim)
+print(type(nim))
